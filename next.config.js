b6c2b983d6088b9c845963c526ab73b07334e597
ParkHyunKyeong <kj3477417@gmail.com>
2023-06-21 17:07:43 +0900

    feature: API키를 rewirtes 함수를 이용해서 숨기고 영화 목록 페이지 API 연결 및 Css 적용

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = '09049690cf5bf59acf12fda802f3bbc6';
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/contact/:path*',
+        destination: '/form/:path*',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
